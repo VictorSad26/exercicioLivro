177684cf9f465266ddb4f0e1c8393c1f001b98ef
VictorSad26 <61890850+VictorSad26@users.noreply.github.com>
2020-10-20 21:30:53 -0300

    fazendo o exercicio dos Livros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Livro } from 'src/app/livro.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'exercicioLivro';
+  livros: Livro[] = [];
+  onLivroAdicionado(livro) {
+    this.livros = [...this.livros, livro];
+  }
 }
